e0606f030d8c46017dc1e7e5090cec95a1bf473f
IsmaMH779 <109138248+IsmaMH779@users.noreply.github.com>
2025-02-20 19:57:09 +0100

    Add files via upload

--- src/dao/AcademiaDAOImplJDBC.java
+++ src/dao/AcademiaDAOImplJDBC.java
@@ -15,7 +15,7 @@ import entidades.Matricula;
 
 public class AcademiaDAOImplJDBC implements AcademiaDAO {
 	// Cadena de conexión predeterminada
-		private String URLConexion = new String("jdbc:mysql://tu_IP:3306/dbformacion?user=tu_usuario&password=tu_pwd");
+		private String URLConexion = new String("jdbc:mysql://tu_IP:3306/dbformacion?user=dam2a&password=secreto");
 		
 		/*
 		 * SQL
@@ -103,56 +103,225 @@ public class AcademiaDAOImplJDBC implements AcademiaDAO {
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} finally {
+				try {
+					releaseConnection(getConnection());
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 			}
+			
 			return alumno;
 		}
 
 		@Override
 		public int grabarAlumno(Alumno alumno) {
-			// TODO Auto-generated method stub
-			return 0;
+			String sql = "INSERT INTO alumnos (id_alumno, nombre_alumno) VALUES (?,?);";
+			int changed = 0;
+
+			try (PreparedStatement ps = getConnection().prepareStatement(sql)){
+				
+				ps.setInt(1, alumno.getIdAlumno());
+				ps.setString(2, alumno.getNombreAlumno());
+				
+				changed = ps.executeUpdate();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				try {
+					releaseConnection(getConnection());
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			return changed;
 		}
 
 		@Override
 		public int actualizarAlumno(Alumno alumno) {
-			// TODO Auto-generated method stub
-			return 0;
+			String sql = "UPDATE alumnos SET nombre_alumno = ? WHERE id_alumno = ?;";
+			int changed = 0;
+
+			try (PreparedStatement ps = getConnection().prepareStatement(sql)){
+				
+				ps.setString(1, alumno.getNombreAlumno());
+				ps.setInt(2, alumno.getIdAlumno());
+				
+				changed = ps.executeUpdate();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				try {
+					releaseConnection(getConnection());
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			return changed;
 		}
 
 		@Override
 		public int borrarAlumno(int idAlumno) {
-			// TODO Auto-generated method stub
-			return 0;
+			String sql = "DELETE FROM alumnos WHERE id_alumno = ?;";
+			int changed = 0;
+
+			try (PreparedStatement ps = getConnection().prepareStatement(sql)){
+				
+				ps.setInt(1, idAlumno);
+				
+				changed = ps.executeUpdate();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				try {
+					releaseConnection(getConnection());
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			return changed;
 		}
 
 		@Override
 		public Collection<Curso> cargarCursos() {
-			// TODO Auto-generated method stub
-			return null;
+			Collection<Curso> cursos = new ArrayList<Curso>();
+
+			try (PreparedStatement ps = getConnection().prepareStatement(FIND_ALL_ALUMNOS_SQL);) {
+
+				ResultSet rs = ps.executeQuery();	
+				
+				while (rs.next()) {
+					int id= rs.getInt(1);				
+					String nombre = 
+						(rs.getString(2)!=null?rs.getString(2):"sin nombre");
+					
+					cursos.add(new Curso(id,nombre));
+				}
+			} catch (SQLException e) {
+				for (Throwable t : e) {
+					System.err.println("Error: " + t);
+				}
+			} finally {
+				try {
+					releaseConnection(getConnection());
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			return cursos;
 		}
 
 		@Override
 		public Curso getCurso(int idCurso) {
-			// TODO Auto-generated method stub
-			return null;
+			String sql = "SELECT * FROM cursos WHERE id_curso = ?";
+			Curso curso = null;
+
+			try (PreparedStatement ps = getConnection().prepareStatement(sql)){
+				
+				ps.setInt(1, idCurso);
+				
+				ResultSet res = ps.executeQuery();
+				
+				curso = new Curso(res.getInt("id_curso"), res.getString("nombre_curso"));
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				try {
+					releaseConnection(getConnection());
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			return curso;
 		}
 
 		@Override
 		public int grabarCurso(Curso curso) {
-			// TODO Auto-generated method stub
-			return 0;
+			String sql = "INSERT INTO cursos (id_curso, nombre_curso) VALUES (?,?);";
+			int changed = 0;
+
+			try (PreparedStatement ps = getConnection().prepareStatement(sql)){
+				
+				ps.setInt(1, curso.getIdCurso());
+				ps.setString(2, curso.getNombreCurso());
+				
+				changed = ps.executeUpdate();
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					releaseConnection(getConnection());
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			return changed;
 		}
 
 		@Override
 		public int actualizarCurso(Curso curso) {
-			// TODO Auto-generated method stub
-			return 0;
+			String sql = "UPDATE cursos SET nombre_curso = ? WHERE id_curso = ?;";
+			int changed = 0;
+
+			try (PreparedStatement ps = getConnection().prepareStatement(sql)){
+				
+				ps.setString(1, curso.getNombreCurso());
+				ps.setInt(2, curso.getIdCurso());
+				
+				changed = ps.executeUpdate();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				try {
+					releaseConnection(getConnection());
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			return changed;
 		}
 
 		@Override
 		public int borrarCurso(int idCurso) {
-			// TODO Auto-generated method stub
-			return 0;
+			String sql = "DELETE FROM cursos WHERE id_curso = ?;";
+			int changed = 0;
+
+			try (PreparedStatement ps = getConnection().prepareStatement(sql)){
+				
+				ps.setInt(1, idCurso);
+				
+				changed = ps.executeUpdate();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				try {
+					releaseConnection(getConnection());
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			return changed;
 		}
 
 		@Override
